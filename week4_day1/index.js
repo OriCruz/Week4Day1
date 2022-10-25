84f7e77b0c9d3d73ebb3c6babc68d61d858a31ba
OriCruz <oc080901@gmail.com>
2022-10-25 13:25:04 -0400

    FizzBuzz answered

--- week4_day1/index.js
+++ week4_day1/index.js
@@ -11,26 +11,26 @@
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
-const fizzBuzz = () =>{
-    let num;
-    for(let i=1; i<101; i++){
-        num=i;
+// const fizzBuzz = () =>{
+//     let num;
+//     for(let i=1; i<101; i++){
+//         num=i;
     
-    if(num%3===0 && num%5===0){
-        console.log(`FizzBuzz`);
-    }
-    else if(num%3===0){
-        console.log(`Fizz`);
-    }
-    else if(num%5===0){
-        console.log(`Buzz`);
-    }
-    else {
-        console.log(num);
-    }
-}
-}
-fizzBuzz();
+//     if(num%3===0 && num%5===0){
+//         console.log(`FizzBuzz`);
+//     }
+//     else if(num%3===0){
+//         console.log(`Fizz`);
+//     }
+//     else if(num%5===0){
+//         console.log(`Buzz`);
+//     }
+//     else {
+//         console.log(num);
+//     }
+// }
+// }
+// fizzBuzz();
 
 ////////////////////////////////
 // Fizz Buzz
